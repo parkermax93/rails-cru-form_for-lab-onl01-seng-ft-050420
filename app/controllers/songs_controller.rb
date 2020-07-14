192a24f724b98d87c5878f2bc13b024b1593fd71
Parker Williamson <parker.williamson@gmail.com>
2020-07-14 23:53:59 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,28 +1,37 @@
 class SongsController < ApplicationController
+  
     def index
         @songs = Song.all
     end
+    
     def show
         set_song 
     end
+    
     def new
         @song = Song.new
     end
+    
     def create
         @song = Song.create(song_params)
         redirect_to song_path(@song)
     end
+    
     def edit
         set_song
     end
+    
     def update
         @song = Song.update(song_params)
         redirect_to song_path(@song)
     end
+    
 private
+
     def set_song
         @song = Song.find(params[:id])
     end
+    
     def song_params
         params.require(:song).permit(:name)
     end
