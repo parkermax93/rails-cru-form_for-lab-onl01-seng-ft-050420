35e854f0b548dda01f0b2a9cfd360a309c2eac2e
Parker Williamson <parker.williamson@gmail.com>
2020-07-14 22:53:21 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,26 +1,31 @@
 class GenresController < ApplicationController
   
-  def index 
-    
-  end
-  
   def show 
-    
+    @genre = Genre.find(params[:id])
   end
   
   def new 
-    
+    @genre = Genre.new
   end
   
   def create 
-    
+    @genre = Genre.new(genre_params)
+    @genre.save 
+    redirect_to genre_path(@genre)
   end
   
   def edit 
-    
+    @artist = Artist.find(params[:id])
   end
   
   def update 
-    
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params)
+    redirect_to artist_path(@artist)
   end 
+  
+  private 
+  
+  def artist_params 
+    params.require(:artist).permit(:name, :bio)
 end
