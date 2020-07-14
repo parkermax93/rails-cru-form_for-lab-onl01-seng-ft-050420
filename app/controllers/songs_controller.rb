ae17f59268424f26dc29b6417958e710951d8212
Parker Williamson <parker.williamson@gmail.com>
2020-07-14 22:58:58 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,36 +1,36 @@
 class SongsController < ApplicationController
   
   def index 
-    @songs = Artist.all
+    @songs = Song.all
   end
   
   def show 
-    @artist = Artist.find(params[:id])
+    @song = Song.find(params[:id])
   end
   
   def new 
-    @artist = Artist.new
+    @song = Song.new
   end
   
   def create 
-    @artist = Artist.new(artist_params)
-    @artist.save 
-    redirect_to artist_path(@artist)
+    @song = Song.new(song_params)
+    @song.save 
+    redirect_to song_path(@song)
   end
   
   def edit 
-    @artist = Artist.find(params[:id])
+    @song = Song.find(params[:id])
   end
   
   def update 
-    @artist = Artist.find(params[:id])
-    @artist.update(artist_params)
-    redirect_to artist_path(@artist)
+    @song = Song.find(params[:id])
+    @song.update(song_params)
+    redirect_to song_path(@song)
   end 
   
   private 
   
-  def artist_params 
-    params.require(:artist).permit(:name, :bio)
+  def song_params 
+    params.require(:song).permit(:name, :bio)
   end
 end
